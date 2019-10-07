794ce4712489f8462eb7d9bca16799a448c9f563
Jochen <jo@hassfurter.com>
2019-10-07 14:59:53 +0200

    Einfügen der Routes zu den zwei neu erstellen Angular-Komponenten

--- sh-recode/src/app/app-routing.module.ts
+++ sh-recode/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartComponent} from "./start/start.component";
+import { RecodeComponent} from "./recode/recode.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StartComponent },
+  { path: 'recode', component: RecodeComponent },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
