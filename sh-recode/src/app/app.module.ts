cf70142408f2fe1f92b0b6b4831902381dca0945
Jochen <jo@hassfurter.com>
2019-10-07 17:09:07 +0200

    $ npm install primeng --save
    TODO: continue with https://www.tutorialspoint.com/angular_material7/angular_material7_radio_button.htm

--- sh-recode/src/app/app.module.ts
+++ sh-recode/src/app/app.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatRadioModule} from '@angular/material';
 import { StartComponent } from './start/start.component';
 import { RecodeComponent } from './recode/recode.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { RecodeComponent } from './recode/recode.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- sh-recode/src/app/recode/recode.component.ts
+++ sh-recode/src/app/recode/recode.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Dictionary } from './dictionary';
+
 
 @Component({
   selector: 'app-recode',
@@ -7,9 +10,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecodeComponent implements OnInit {
 
-  constructor() { }
+  codes = new Dictionary();
+  modell = new Dictionary();
+  bandbreite = new Dictionary();
+  spannmotor = new Dictionary();
+  transportmotor = new Dictionary();
+  gehaeuse = new Dictionary();
+  verschliesserantrieb = new Dictionary();
+  verschlussart = new Dictionary();
+  frei = new Dictionary();
+  ausrichtung = new Dictionary();
+  zusatzoptionen = new Dictionary();
+
+  fgCode = new FormGroup({
+    fcModell: new FormControl(''),
+    fcBandbreite: new FormControl(''),
+    fcSpannmotor: new FormControl(''),
+    fcTransportmotor: new FormControl(''),
+    fcGehaeuse: new FormControl(''),
+    fcVerschliesserantrieb: new FormControl(''),
+    fcVerschlussart: new FormControl(''),
+    fcFrei: new FormControl(''),
+    fcAusrichtung: new FormControl(''),
+    fcZusatzoptionen: new FormControl('')
+  });
+
+  constructor() {}
 
   ngOnInit() {
+    this.modell.set('SSH', '1');
+    this.modell.set('PSH', '4');
+    this.codes.set('1', this.modell);
+    this.bandbreite.set('32mm', '3');
+    this.bandbreite.set('25mm', '4');
+    this.bandbreite.set('19mm', '5');
+    this.codes.set('2', this.bandbreite);
+    this.spannmotor.set('E (elektrisch)', '1');
+    this.spannmotor.set('P (pneumatisch)', '4');
+    this.spannmotor.set('ohne Spannmotor', '9');
+    this.codes.set('3', this.spannmotor);
+    this.transportmotor.set('E (elektrisch)', '1');
+    this.transportmotor.set('P (pneumatisch)', '4');
+    this.transportmotor.set('ohne Transportmotor', '9');
+    this.codes.set('4', this.transportmotor);
+    this.gehaeuse.set('A (Aluminiumgehäuse', 'A');
+    this.gehaeuse.set('S (Stahlgehäuse', 'S');
+    this.codes.set('5', this.gehaeuse)
+    this.verschliesserantrieb.set('E (elektrisch)', '1');
+    this.verschliesserantrieb.set('P (pneumatisch)', '4');
+    this.codes.set('6', this.verschliesserantrieb);
+    this.verschlussart.set('Kerbverschluss hülsenlos (Stahl)', '1');
+    this.verschlussart.set('mit Hülse (Stahl/Kunststoff)', '2');
+    this.verschlussart.set('Punkt-Schweißverschluss (Stahl/Kunststoff)', '4');
+    this.verschlussart.set('WIG-Schweißverschluss (Stahl)', '5');
+    this.verschlussart.set('Reibschweißverschluss (Kunststoff)', '7');
+    this.verschlussart.set('Heizkeilverschluss (Kunststoff)', '8');
+    this.verschlussart.set('Ultraschallverschluss (Kunststoff)', '9');
+    this.codes.set('7', this.verschlussart);
+    this.frei.set('', '0');
+    this.codes.set('8', this.frei);
+    this.ausrichtung.set('R (Antriebseinheit rechts)', 'R');
+    this.ausrichtung.set('L (Antriebseinheit links)', 'L');
+    this.codes.set('9', this.ausrichtung);
+    this.zusatzoptionen.set('ohne Sicherheitskerbe', '1');
+    this.zusatzoptionen.set('SK (mit Sicherheitskerbe', '3');
+    this.zusatzoptionen.set('Typ AM (Typ Amova)', 'T');
+    this.codes.set('10', this.zusatzoptionen);
   }
-
 }
